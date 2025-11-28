80a3f0f42ac0cc9865fe6833b4030cb4a27a2bf2
jeyboy22 <yjkenoy@gmail.com>
2025-11-28 21:08:48 +0530

    new changes

--- folder/app/app.py
+++ folder/app/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException 
+from app.schemas import PostCreate
 
 app = FastAPI()
 
@@ -19,7 +20,7 @@ text_posts = {
 @app.get("/posts")
 def get_all_posts(limit: int = None):
     if limit:
-        return list(text_posts.values())[:limit]
+        return list(text_posts.values())  [:limit]
     return text_posts
 
 
@@ -29,3 +30,9 @@ def get_post(id: int):
     if id not in text_posts:
         raise HTTPException(status_code=404,detail="Post not found")
     return text_posts.get(id)
+
+@app.post("/posts")
+def create_post(post: PostCreate):
+    new_post ={"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
