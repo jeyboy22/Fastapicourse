2fa6d03afd64de3a99ce1335f80cdc230fedc8aa
jeyboy22 <yjkenoy@gmail.com>
2025-12-01 21:00:33 +0530

    sql lite

--- folder/app/userinfo/auth_api.py
+++ folder/app/userinfo/auth_api.py
@@ -1,42 +1,48 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Session, create_engine, select
+
 
 app = FastAPI()
 
-users_db = {}
+sqlite_file_name = "users.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-class Register(BaseModel):
-    username: str
+engine = create_engine(sqlite_url, echo=True)
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None,primary_key=True)
+    username: str = Field(index=True, unique=True)
     password: str
 
-class Login(BaseModel):
-    username: str
-    password: str
-
-class UserInfo(BaseModel):
-    username: str
+SQLModel.metadata.create_all(engine)
 
 @app.post("/register")
-def register(user: Register):
-    if user.username in users_db:
-        raise HTTPException(status_code=400, detail="Username already exists")
-
-    users_db[user.username] = user.password
-    return {"message": "User registered successfully!"}
+def register(user: User):
+  with Session(engine) as session:
+        statement = select(User).where(User.username == user.username)
+        existing_user = session.exec(statement).first()
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already exists")
+        
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return {"message": "User registered successfully!"}
 
 @app.post("/login")
-def login(user: Login):
-    if user.username not in users_db:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    if users_db[user.username] != user.password:
-        raise HTTPException(status_code=401, detail="Incorrect password")
-
-    return {"message": "Login successful"}
+def login(user: User):
+    with Session(engine) as session:
+        statement = select(User).where(User.username == user.username)
+        db_user = session.exec(statement).first()
+        if not db_user or db_user.password != user.password:
+            raise HTTPException(status_code=401, detail="Invalid username or password")
+        return {"message": "Login successful"}
 
 @app.get("/user/{username}")
 def get_user(username: str):
-    if username not in users_db:
-        raise HTTPException(status_code=404, detail="User does not exist")
-
-    return {"username": username, "status": "Active"}
+    with Session(engine) as session:
+        statement = select(User).where(User.username == username)
+        db_user = session.exec(statement).first()
+        if not db_user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return {"username": db_user.username, "status":"Active"}
